3f6af3131f836248b1e46912979d8ab492a9eb80
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2025-12-19 16:51:11 +0900

    add HF_TOKEN to MEMO.md and update run.sh permissions

--- run_eval.sh
+++ run_eval.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# 環境変数の設定
-export NCCL_DEBUG=WARN
-export OMP_NUM_THREADS=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+python script/inference.py --model_type avsr_cocktail --session_dir "data-bin/dev/*" \
+    --checkpoint_path ./model-bin/avsr_cocktail \
+    --beam_size 3 --max_length 15 \
+    --output_dir_name output_avsr_cocktail
 
-# 推論実行
-python script/inference_face.py \
-    --model_type avsr_cocktail \
-    --session_dir "data-bin/dev/*" \
-    --checkpoint_path ./model-bin/mcorec_finetuning \
-    --beam_size 3 \
-    --max_length 15 \
-    --output_dir_name output_finetuning_face 
-
-# 評価実行
-python script/evaluate.py \
-    --session_dir "data-bin/dev/*" \
-    --output_dir_name output_finetuning_face  
-
+python script/evaluate.py --session_dir "data-bin/dev/*" --output_dir_name output_avsr_cocktail
